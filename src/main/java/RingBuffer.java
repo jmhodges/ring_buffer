1b66c271b2f67dfbd95fdb451ec169763e3da967
Jeff Hodges <jeff@somethingsimilar.com>
2011-01-10 00:21:42 -0800

    whitespace in comment in RingBuffer

--- src/main/java/RingBuffer.java
+++ src/main/java/RingBuffer.java
@@ -1,7 +1,7 @@
 package com.somethingsimilar.ring_buffer;
 
 // Here because I'm assuming I'll have to do interesting things when writing
-//  the object pre-allocating version of the AtomicRingBuffer.
+// the object pre-allocating version of the AtomicRingBuffer.
 public interface RingBuffer<T> {
   public long add(T obj);
   public T get(long slot);
